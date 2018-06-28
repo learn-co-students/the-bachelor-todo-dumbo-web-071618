c53b50d901f24471471e4d242173a9db2716f6b9
Bryan Teng <bteng1226@gmail.com>
2018-06-28 05:40:53 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,8 +1,8 @@
 def get_first_name_of_season_winner(data, season)
-string = string.to_sym
+season = season.to_sym
 data.each do |season_number, seasons_hash| 
   seasons_hash.each do |contestant|
-    if season_number == string
+    if season_number == season
       if contestant[:status] == "Winner"
         return contestant[:name].split(" ")[0]
       end
