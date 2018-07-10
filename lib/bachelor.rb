40de838f46e975c63ca2f3d7a613983e25aa5918
github username <github email address>
2018-07-10 13:48:26 -0400

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,79 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |season_number, contestant_data_array|
+    if season_number == season
+      contestant_data_array.each do |contestant_data|
+        contestant_data.each do |label, detail|
+          if detail == "Winner"
+            contestant_data.each do |label, detail|
+              return detail.split(" ")[0]
+            end
+          end
+        end
+      end
+    end
+  end
 end
 
-def get_contestant_name(data, occupation)
-  # code here
+def get_contestant_name (data, occupation)
+  data.each do |season_number, contestant_data_array|
+    contestant_data_array.each do |contestant_data|
+      contestant_data.each do |label, detail|
+        if occupation == detail
+          contestant_data.each do |label, detail|
+            return detail
+          end
+        end
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  contestants_in_hometown = 0
+  data.each do |season_number, contestant_data_array|
+    contestant_data_array.each do |contestant_data|
+      contestant_data.each do |label, detail|
+        if hometown == detail
+          contestants_in_hometown += 1
+        end
+      end
+    end
+  end
+contestants_in_hometown
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season_number, contestant_data_array|
+    contestant_data_array.each do |contestant_data|
+      contestant_data.each do |label, detail|
+        if hometown == detail
+          contestant_data.each do |label, detail|
+            if label == "occupation"
+              return detail
+            end
+          end
+        end
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age_total = 0
+  number_of_people = 0
+  data.each do |season_number, contestant_data_array|
+    if season_number == season
+      contestant_data_array.each do |contestant_data|
+        contestant_data.each do |label, detail|
+          if label == "age"
+            age_total = age_total + detail.to_f
+            number_of_people += 1
+          end
+        end
+      end
+    end
+  end
+(age_total / number_of_people).round
 end
